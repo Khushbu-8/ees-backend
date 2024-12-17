159ba65e0b36da2c48dfd40cc551376ae17484e6
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-17 13:33:49 +0530

    testy

--- index.js
+++ index.js
@@ -9,10 +9,8 @@ const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = {
-    origin: 'https://ess-frontend-eight.vercel.app', // Allow only your frontend domain
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'], // Allowed HTTP methods
-  allowedHeaders: ['Content-Type', 'Authorization'], // Allowed headers
-  credentials: true, // If using cookies or authorization headers
+    origin: 'https://ess-frontend-eight.vercel.app', // Client URL
+    credentials: true, // Allow credentials (cookies, headers, etc.)
 };
 app.use(cors(corsOptions));
 app.use(express.json());
