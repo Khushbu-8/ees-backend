01219084396e48a96525fb715300e16d0ae08a18
khushbu <147253120+Khushbu-8@users.noreply.github.com>
2024-12-23 09:22:52 +0530

    Update index.js

--- index.js
+++ index.js
@@ -10,7 +10,6 @@ const cookieParser = require("cookie-parser");
 const cors = require("cors");
 const corsOptions = {
     origin: 'https://ess-frontend-eight.vercel.app', // Client URL
-    // origin: 'http://localhost:5173', // Client URL
     credentials: true, // Allow credentials (cookies, headers, etc.)
 };
 app.use(cors(corsOptions));
