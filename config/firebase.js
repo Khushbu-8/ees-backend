cbbb40d9d28bad41acb3b4a2aa0f89d0f65d9873
khushbu-8 <khushbuzalavadiya8@gmail.com>
2024-12-31 10:18:11 +0530

    fcm

--- config/firebase.js
+++ config/firebase.js
@@ -1,12 +1,21 @@
-
-require('dotenv').config()
+require('dotenv').config();
 const admin = require("firebase-admin");
-const serviceAccount = require('./serviceAccountKey.json'); // Adjust the path if needed
-  
-// const serviceAccount = process.env.FIREBASE_ADMIN_KEY;
 
-if (!serviceAccount) {
-  throw new Error("FIREBASE_ADMIN_KEY environment variable is not set.");
+const serviceAccount = {
+  type: process.env.FIREBASE_TYPE,
+  project_id: process.env.FIREBASE_PROJECT_ID,
+  private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+  private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+  client_email: process.env.FIREBASE_CLIENT_EMAIL,
+  client_id: process.env.FIREBASE_CLIENT_ID,
+  auth_uri: process.env.FIREBASE_AUTH_URI,
+  token_uri: process.env.FIREBASE_TOKEN_URI,
+  auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+  client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+};
+
+if (!serviceAccount.private_key || !serviceAccount.client_email) {
+  throw new Error("Firebase credentials are not properly set in environment variables.");
 }
 
 admin.initializeApp({
